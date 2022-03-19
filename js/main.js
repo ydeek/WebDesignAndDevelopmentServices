c6b39fcc70263071d8e6e521d6e50aee6da7eaaf
Yazan Deek <ydeek91@yahoo.com>
2022-03-19 10:10:02 -0700

    completed developing about us section

--- js/main.js
+++ js/main.js
@@ -9,6 +9,13 @@ $(window).on('load scroll', function(){
     $('.fa-bars').removeClass('fa-times');
     $('.navbar').removeClass('nav-toggle');
 
+
+    if($(window).scrollTop() > 30){
+     $('.header').css({'background':'#6c5ce7','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+    }else{
+        $('.header').css({'background':'none','box-shadow':'none'});
+    }
+
 });
 
 });
